01c94dbc7b94665a523f568ffd2ee0b9d676efeb
Ralph Schindler <ralph@ralphschindler.com>
2014-03-27 14:52:33 -0500

    Fixed the Unauthorized listener to pass on headers from the MvcEvent Response Added missing unit test for UnauthorizedListener, with test for fix

---
+++ test/MvcAuth/UnauthorizedListenerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ZFTest\Apigility\MvcAuth;
+
+use Zend\Http\Header\WWWAuthenticate;
+use Zend\Http\Response;
+use Zend\Mvc\MvcEvent;
+use ZF\Apigility\MvcAuth\UnauthorizedListener;
+use ZF\MvcAuth\MvcAuthEvent;
+
+class UnauthorizedListenerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ZF\Apigility\MvcAuth\UnauthorizedListener::__invoke
+     */
+    public function testInvoke()
+    {
+        $unauthorizedListener = new UnauthorizedListener();
+
+        $mvcEvent = new MvcEvent();
+        $mvcEvent->setResponse(new Response);
+
+        $mvcAuthEvent = new MvcAuthEvent($mvcEvent, null, null);
+        $mvcAuthEvent->setIsAuthorized(false);
+
+        $invokeResponse = $unauthorizedListener->__invoke($mvcAuthEvent);
+        $this->assertInstanceOf('ZF\ApiProblem\ApiProblemResponse', $invokeResponse);
+        $this->assertEquals(403, $invokeResponse->getStatusCode());
+        $this->assertEquals('Forbidden', $invokeResponse->getReasonPhrase());
+    }
+
+    /**
+     * @covers ZF\Apigility\MvcAuth\UnauthorizedListener::__invoke
+     */
+    public function testInvokeWillPropagateMvcEventResponseHeaders()
+    {
+        $unauthorizedListener = new UnauthorizedListener();
+
+        $mvcEvent = new MvcEvent();
+
+        $mvcEventResponse = new Response();
+        $mvcEventResponse->getHeaders()->addHeader(new WWWAuthenticate());
+        $mvcEvent->setResponse($mvcEventResponse);
+
+        $mvcAuthEvent = new MvcAuthEvent($mvcEvent, null, null);
+        $mvcAuthEvent->setIsAuthorized(false);
+
+        $invokeResponse = $unauthorizedListener->__invoke($mvcAuthEvent);
+        $this->assertInstanceOf('ZF\ApiProblem\ApiProblemResponse', $invokeResponse);
+
+        $wwwAuthHeader = $invokeResponse->getHeaders()->get('WWW-Authenticate');
+        $this->assertInstanceOf('Zend\Http\Header\WWWAuthenticate', $wwwAuthHeader[0]);
+    }
+}
+ 
